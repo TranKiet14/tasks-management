05e2d2034f736ac126ff19c90d01cc77f253a539
Trần Anh Kiệt <trananhkiet14032002@gmail.com>
2023-11-30 15:57:11 +0700

    tasks-management

---
+++ api/v1/routes/task.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const Task = require("../models/task.model");
+const controller = require("../controllers/task.controller");
+
+router.get("/", controller.index);
+
+router.get("/detail/:id", controller.detail);
+
+router.patch("/change-status/:id", controller.changeStatus)
+
+router.patch("/change-multi", controller.changeMulti)
+
+router.post("/create", controller.create)
+
+router.patch("/edit/:id", controller.edit)
+
+router.delete("/delete/:id", controller.delete)
+
+module.exports = router;
